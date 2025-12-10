0faad26aa6f4713ff6ab1a5657d540a09bd9d99a
orbz <orbzs085@gmail.com>
2025-12-10 13:52:40 +0900

    /api/attraction/{attractionId}

--- app.py
+++ app.py
@@ -1,17 +1,61 @@
 from fastapi import *
 from fastapi.responses import FileResponse
+
+from typing import Annotated
+from fastapi import FastAPI, Query
+
 app=FastAPI()
+
+import mysql.connector
+
+con = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="mysql",
+  database="tpidaytrip"
+)
 
 # Static Pages (Never Modify Code in this Block)
 @app.get("/", include_in_schema=False)
 async def index(request: Request):
-	return FileResponse("./static/index.html", media_type="text/html")
+    return FileResponse("./static/index.html", media_type="text/html")
 @app.get("/attraction/{id}", include_in_schema=False)
 async def attraction(request: Request, id: int):
-	return FileResponse("./static/attraction.html", media_type="text/html")
+    return FileResponse("./static/attraction.html", media_type="text/html")
 @app.get("/booking", include_in_schema=False)
 async def booking(request: Request):
-	return FileResponse("./static/booking.html", media_type="text/html")
+    return FileResponse("./static/booking.html", media_type="text/html")
 @app.get("/thankyou", include_in_schema=False)
 async def thankyou(request: Request):
-	return FileResponse("./static/thankyou.html", media_type="text/html")
+    return FileResponse("./static/thankyou.html", media_type="text/html")
+
+
+@app.get("/api/attraction/{attractionId}")
+async def searchid(attractionId: int):
+    cursor = con.cursor(dictionary=True)
+    cursor.execute("""
+                SELECT * FROM attractions WHERE id=%s
+                """, (attractionId,))
+    result = cursor.fetchone()
+    
+    if result == None:
+        return {
+            "error": True,
+            "message": "景點編號不存在"
+        }
+    
+    cursor.execute("""
+                SELECT images.image_url
+                FROM attractions
+                JOIN images on images.attraction_id = attractions.id
+                WHERE attractions.id = %s;
+                """, (attractionId,))
+    result_img = cursor.fetchall()
+    con.close()
+    
+    imgurl_list = []
+    for url in result_img:
+        imgurl_list.append(url["image_url"])
+    result["image"]=imgurl_list
+
+    return {"data": result}
---
+++ data/sql.py
@@ -0,0 +1,50 @@
+import json
+import re
+import mysql.connector
+
+with open ("./taipei-attractions.json", 'r', encoding='utf-8') as file:
+    raw = json.load(file)
+data = raw["result"]["results"]
+
+con = mysql.connector.connect(
+  host = "localhost",
+  user = "root",
+  password = "mysql",
+  database = "tpidaytrip"
+)
+
+cursor = con.cursor()
+
+sql_query_att = """
+    INSERT INTO attractions 
+    (name, category, description, address, transport, mrt, lat, lng)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+"""
+sql_query_img = """
+    INSERT INTO images 
+    (attraction_id, image_url)
+    VALUES (%s, %s)
+"""
+pattern = r'https?://.*?\.(?:jpg|JPG|png|PNG)'
+
+for attraction in data:
+    values_att = (
+        attraction["name"],
+        attraction["CAT"],
+        attraction["description"],
+        attraction["address"],
+        attraction["direction"],
+        attraction["MRT"] if attraction["MRT"] else None,
+        float(attraction["latitude"]),
+        float(attraction["longitude"]),
+    )
+    cursor.execute(sql_query_att, values_att)
+
+    att_id = cursor.lastrowid
+    image_urls = re.findall(pattern, attraction["file"])
+    for url in image_urls:
+        cursor.execute(sql_query_img, (att_id, url))
+
+con.commit()
+cursor.close()
+con.close()
